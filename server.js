2b0ff386bdc098ffbf5d244b9426e650cd726e3b
azzpronos <contactmoifr@gmail.com>
2026-03-28 23:23:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: true,
+secure: true,
+httpOnly: true,
 sameSite: 'none',
-    httpOnly: true,
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 jours
   }
 }));
